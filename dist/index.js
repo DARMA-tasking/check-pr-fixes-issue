9835ecd407d19136d35a2ffa8222ab47792b7e93
Jakub Strzebonski <jakub.strzebonski@ng-analytics.com>
2022-03-01 20:20:08 +0100

    #12: allow underscores in branch name

--- dist/index.js
+++ dist/index.js
@@ -467,7 +467,7 @@ const core = __webpack_require__(186);
 function checkPrBranch(prBranch) {
   core.info(`Checking branch name formatting\n  - "${prBranch}"`);
 
-  let prBranchRegexp = /^\d+(-[^\W_]+)+$/;
+  let prBranchRegexp = /^\d+([-_][^\W_]+)+$/;
   return prBranchRegexp.test(prBranch);
 }
 
@@ -478,7 +478,8 @@ function checkPrTitle(prTitle) {
   return prTitleRegexp.test(prTitle);
 }
 
-const prDescriptionRegexp = /((fix(e[ds])?)|(close[ds]?)|(resolve[ds]?))(:? (DARMA-tasking\/[\w-]+)?#)\d+/i;
+const prDescriptionRegexp =
+  /((fix(e[ds])?)|(close[ds]?)|(resolve[ds]?))(:? (DARMA-tasking\/[\w-]+)?#)\d+/i;
 
 function checkPrDescription(prDescription) {
   core.info(`Checking PR description formatting\n  - "${prDescription}"`);
